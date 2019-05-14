a58f95b1e928406ec378a7eb95ee71fb255bebf2
user <user@IN302>
2019-05-14 10:43:08 +0900

    first

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -35,7 +35,6 @@ public class HomeController {
 	@GetMapping("/users/{id}")
 	public String getUserById(@PathVariable(value = "id") Long userId,  
 	Model model) throws ResourceNotFoundException {
-		//User user = userRepo.findById(userId).get();
 		User user = userRepo.findById(userId)
 				.orElseThrow(() -> 
 				new ResourceNotFoundException("not found " + userId ));
